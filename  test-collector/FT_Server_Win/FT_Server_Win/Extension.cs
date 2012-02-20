b16013723b8abb3b1a690751b26901248e8c14f4
gentlerain2408 <gentlerain2408@4d3169a4-5381-785a-1a34-a76a2f7390c1>
2012-02-20 17:19:45 +0000

    + Thêm form ServerUI (giao diện chính) + Thêm ServerSocketControl

---
+++  test-collector/FT_Server_Win/FT_Server_Win/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace FT_Server_Win
+{
+    public static class Extension
+    {
+        public static DataTable CreateDataTableWithHeader()
+        {
+            DataTable dataTableResult = new DataTable();
+            dataTableResult.Columns.Add("STT", typeof(int));
+            dataTableResult.Columns.Add("Tên máy", typeof(string));
+            dataTableResult.Columns.Add("Địa chỉ IP", typeof(string));
+            dataTableResult.Columns.Add("Mã số sinh viên", typeof(string));
+            dataTableResult.Columns.Add("Họ tên sinh viên", typeof(string));
+            dataTableResult.Columns.Add("Trạng thái", typeof(string));
+
+            return dataTableResult;
+        }
+    }
+}
